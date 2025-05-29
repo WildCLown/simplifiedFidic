605617247446c97d2ae7aced63755ee6d5197e2b
Gabriel Teixeira <gtsa.wildclown@gmail.com>
2025-05-29 00:37:45 -0300

    Lint

--- SimplifiedFidic/src/main/kotlin/br/com/baycap/simplifiedfidic/model/FidicOperation.kt
+++ SimplifiedFidic/src/main/kotlin/br/com/baycap/simplifiedfidic/model/FidicOperation.kt
@@ -7,7 +7,8 @@ enum class FidicOperation(val id: Int) {
     UNKNOWN(0),
     RECEIVE_CASH(1),
     REGISTER_DEFAULT(2),
-    DISTRIBUTE_CASH(3);
+    DISTRIBUTE_CASH(3),
+    ;
 
     @JsonValue
     fun toJson(): String = name.uppercase()
